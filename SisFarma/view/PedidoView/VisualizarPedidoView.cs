a1e5f970a0cd8cfecd14c01b793d34f66d10a1b8
Lucas Mendes <lucas.mendes147@live.com>
2020-05-11 14:44:30 -0300

    Finalizado datagrids de pedido

--- SisFarma/view/PedidoView/VisualizarPedidoView.cs
+++ SisFarma/view/PedidoView/VisualizarPedidoView.cs
@@ -11,6 +11,8 @@ namespace SisFarma.view.PedidoView
         private PedidoController pedidoController;
         private CurrentIdController current;
         private DataTable dt;
+        private DataTable dt1;
+        private DataTable dt2;
         private int rowSelected;
         public VisualizarPedidoView()
         {
@@ -18,6 +20,8 @@ namespace SisFarma.view.PedidoView
             this.pedidoController = new PedidoController();
             this.current = new CurrentIdController();
             this.dt = new DataTable();
+            this.dt1 = new DataTable();
+            this.dt2 = new DataTable();
         }
 
         private void inicializarataTable()
@@ -28,6 +32,22 @@ namespace SisFarma.view.PedidoView
             dt.Columns.Add("Valor Total");            
             dataGridView1.DataSource = dt;
             dataGridView1.Columns["Id"].Visible = false;
+
+            dt1.Columns.Add("Id");
+            dt1.Columns.Add("Data Pedido");
+            dt1.Columns.Add("Itens");
+            dt1.Columns.Add("Valor Total");
+            dataGridView2.DataSource = dt1;
+            dataGridView2.Columns["Id"].Visible = false;
+
+
+            dt2.Columns.Add("Id");
+            dt2.Columns.Add("Data Pedido");
+            dt2.Columns.Add("Itens");
+            dt2.Columns.Add("Valor Total");
+            dataGridView3.DataSource = dt2;
+            dataGridView3.Columns["Id"].Visible = false;
+
             this.inicializarRows();
         }
 
@@ -42,6 +62,26 @@ namespace SisFarma.view.PedidoView
                 row["Valor Total"] = p.ValorTotal;
                 dt.Rows.Add(row);
             }
+
+            foreach (Pedido p in pedidoController.recuperarTodos(2))
+            {
+                DataRow row = dt1.NewRow();
+                row["Id"] = p.Id;
+                row["Data Pedido"] = p.DataPedido.ToString();
+                row["Itens"] = "";
+                row["Valor Total"] = p.ValorTotal;
+                dt1.Rows.Add(row);
+            }
+
+            foreach (Pedido p in pedidoController.recuperarTodos(3))
+            {
+                DataRow row = dt2.NewRow();
+                row["Id"] = p.Id;
+                row["Data Pedido"] = p.DataPedido.ToString();
+                row["Itens"] = "";
+                row["Valor Total"] = p.ValorTotal;
+                dt2.Rows.Add(row);
+            }
         }
 
         private int retornarId()
@@ -49,6 +89,5 @@ namespace SisFarma.view.PedidoView
             DataGridViewRow selectedRow = dataGridView1.Rows[rowSelected];
             return Convert.ToInt32(selectedRow.Cells["Id"].Value);
         }
-
     }
 }
